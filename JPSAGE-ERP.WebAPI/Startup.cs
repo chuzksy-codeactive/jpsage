d134a46bdbac20e82b4c10eccd09fcca8ddf661f
Chika Onuchukwu <conuchukwu@imperotechne.com>
2020-11-20 09:00:39 +0100

    Refactor code

--- JPSAGE-ERP.WebAPI/Startup.cs
+++ JPSAGE-ERP.WebAPI/Startup.cs
@@ -1,12 +1,11 @@
+using Hangfire;
 using JPSAGE_ERP.Infrastructure.IoC;
-using JPSAGE_ERP.Application.Helpers;
 using JPSAGE_ERP.WebAPI.Installers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Hangfire;
 
 namespace JPSAGE_ERP.WebAPI
 {
@@ -51,15 +50,23 @@ namespace JPSAGE_ERP.WebAPI
             }
 
             app.UseCors("EnableCORS");
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
             app.UseHangfireDashboard();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
 
