3cfd0ccc72d4fc10895dcde5eb9f05e867bf4b55
Chika Onuchukwu <conuchukwu@imperotechne.com>
2020-11-23 12:20:09 +0100

    add endpoint to fetch all projects

---
+++ JPSAGE-ERP.Application/Models/Projects/ProjectsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JPSAGE_ERP.Application.Models.Projects
+{
+    public class ProjectsDto
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ JPSAGE-ERP.WebAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using JPSAGE_ERP.Application.Interfaces;
+using JPSAGE_ERP.Application.Models.Projects;
+using JPSAGE_ERP.Application.Models.Responses;
+using JPSAGE_ERP.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JPSAGE_ERP.WebAPI.Controllers
+{
+    [Route("api/v1/projects")]
+    [ApiController]
+    [Authorize]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IRepository<TblProjects> _tblProjects;
+        private readonly IMapper _mapper;
+        public ProjectsController(IRepository<TblProjects> tblProjects,
+            IMapper mapper)
+        {
+            _tblProjects = tblProjects;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(SucessResponse<IEnumerable<ProjectsDto>>), 200)]
+        public async Task<IActionResult> GetProjects()
+        {
+            try
+            {
+                var projects = await _tblProjects.GetAllAsync();
+
+                var projectsDto = _mapper.Map<IEnumerable<ProjectsDto>>(projects);
+
+                return Ok(new SucessResponse<IEnumerable<ProjectsDto>>
+                {
+                    success = true,
+                    message = "Projects retrieved successfully",
+                    data = projectsDto
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, Response<string>.InternalError(ex.Message));
+            }
+        }
+    }
+}
